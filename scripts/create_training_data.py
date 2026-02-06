fde2698d6448d226e2f6f7891de51a5ba475e74e
Daniel <seisi3322@gmail.com>
2026-02-06 14:58:59 +0000

    font_scripts ready

--- scripts/create_training_data.py
+++
@@ -1,5 +0,0 @@
-import os
-
-training_data_dir = os.path.join(os.path.dirname(os.getcwd()), "training_data")
-
-
--- scripts/start_fontforge.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Building Custom Font Using FontForge..."
-fontforge -script /workspaces/SynthMap/scripts/create_custom_font.py
-echo "Done Building Custom Font."
